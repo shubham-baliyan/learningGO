2128760a466360e901e89d4d7a687434f4c2eff7
shubham baliyan <shubham.baliyan204@gmail.com>
2023-07-01 12:40:59 +0100

    raw string literal exercise

---
+++ Exercise1/exercise1.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+//this excercise is about RAW-STRING-Literal
+
+func main() {
+
+	fmt.Println(`
+	hi 
+	how
+	are you
+	john
+	fleming	
+	`)
+
+	//using backquotes character
+}
